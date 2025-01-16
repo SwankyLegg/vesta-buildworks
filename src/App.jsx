fb60b9fda67bf9a24813d8faa96fa41b233ff6cc
Andrew Swank <adswank@gmail.com>
2025-01-16 12:01:24 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BrandGuidelines from './pages/BrandGuidelines';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <BrandGuidelines />
+    </div>
+  );
+}
+
+export default App; 
